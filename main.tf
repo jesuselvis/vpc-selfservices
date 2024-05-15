0870b137cd30262c451ac7f19b202571d5f1c7bd
jesusqm <tic-jquispe@cajalosandes.pe>
2024-05-15 01:06:54 -0500

    change source git

--- main.tf
+++ main.tf
@@ -8,11 +8,11 @@ terraform {
 
 provider "aws" {
   region     = "us-east-1"
-  profile = "devops"
+#   profile = "devops"
 }
 
 module "vpc" {
-  source = "git::ssh://git@bitbucket.org/anka-escuelita/jesus-aws-vpc.git"#no cambiar
+  source = "git::https://github.com/jesuselvis/vpc-module.git"#no cambiar
   vpc_cidr_block = "10.0.0.0/16"
   subnet_cidr_block_pub = ["10.0.1.0/24"] #you can add two more subnets
   subnet_cidr_block_priv = ["10.0.2.0/24"]
